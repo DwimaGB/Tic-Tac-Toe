d94ead7b771810fae2c988cf733072a69edc55ce
Dwima Basumatary <dwima1583@gmail.com>
2023-08-27 14:59:30 +0530

    fixed bug added tie feature

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,41 +8,41 @@ export default function GameBoard() {
     const [playerMoves, setPlayerMoves] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
 
-    const updateBoard = index=>{
-        if(playerMoves[index] || checkWinner(playerMoves)){
+    const updateBoard = index => {
+        if (playerMoves[index] || checkWinner(playerMoves)) {
             return;
         }
         const newPlayerMoves = [...playerMoves];
 
-        if(xIsNext){
+        if (xIsNext) {
             newPlayerMoves[index] = 'X';
             setXIsNext(false);
         }
-        else{
+        else {
             newPlayerMoves[index] = 'O';
             setXIsNext(true);
         }
 
         setPlayerMoves(newPlayerMoves);
-        
+
     }
 
     return (
         <>
             <div className="board-row">
-                <Square value={playerMoves[0]} updateBoard={()=> updateBoard(0)}></Square>
-                <Square value={playerMoves[1]} updateBoard={()=> updateBoard(1)}></Square>
-                <Square value={playerMoves[2]} updateBoard={()=> updateBoard(2)}></Square>
+                <Square value={playerMoves[0]} updateBoard={() => updateBoard(0)}></Square>
+                <Square value={playerMoves[1]} updateBoard={() => updateBoard(1)}></Square>
+                <Square value={playerMoves[2]} updateBoard={() => updateBoard(2)}></Square>
             </div>
             <div className="board-row">
-                <Square value={playerMoves[3]} updateBoard={()=> updateBoard(3)}></Square>
-                <Square value={playerMoves[4]} updateBoard={()=> updateBoard(4)}></Square>
-                <Square value={playerMoves[5]} updateBoard={()=> updateBoard(5)}></Square>
+                <Square value={playerMoves[3]} updateBoard={() => updateBoard(3)}></Square>
+                <Square value={playerMoves[4]} updateBoard={() => updateBoard(4)}></Square>
+                <Square value={playerMoves[5]} updateBoard={() => updateBoard(5)}></Square>
             </div>
             <div className="board-row">
-                <Square value={playerMoves[6]} updateBoard={()=> updateBoard(6)}></Square>
-                <Square value={playerMoves[7]} updateBoard={()=> updateBoard(7)}></Square>
-                <Square value={playerMoves[8]} updateBoard={()=> updateBoard(8)}></Square>
+                <Square value={playerMoves[6]} updateBoard={() => updateBoard(6)}></Square>
+                <Square value={playerMoves[7]} updateBoard={() => updateBoard(7)}></Square>
+                <Square value={playerMoves[8]} updateBoard={() => updateBoard(8)}></Square>
             </div>
 
             <GameStatus status={updateStatus(checkWinner, xIsNext, playerMoves)}></GameStatus>
@@ -50,7 +50,7 @@ export default function GameBoard() {
     );
 }
 
-function checkWinner(playerMoves){
+function checkWinner(playerMoves) {
     const winnerConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -59,24 +59,29 @@ function checkWinner(playerMoves){
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6]
+        [2, 4, 6],
     ]
     for(const condition of winnerConditions){
         const [i, j, k] = condition;
 
-        if(playerMoves[i] === playerMoves[j] && playerMoves[i] === playerMoves[k]){
+        if(playerMoves[i] && playerMoves[i] === playerMoves[j] && playerMoves[i] === playerMoves[k]){
             return playerMoves[i];
         }
     }
-
-    return null; 
+    return null;
 }
 
-function updateStatus(checkWinner, xIsNext, playerMoves){
+function updateStatus(checkWinner, xIsNext, playerMoves) {
     const winner = checkWinner(playerMoves);
+    const moveRemains = playerMoves.some(el => el === null)
 
-    if (winner){
+    if (winner) {
         return `Player ${winner} won`;
     }
-    return xIsNext? 'Next Player: X': 'Next Player: O';
+
+    if (!winner && !moveRemains) {
+        return `Its a Tie!!`;
+    }
+
+    return xIsNext ? 'Next Player: X' : 'Next Player: O';
 }
